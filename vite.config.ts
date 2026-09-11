1cbd6ce35a27816e2629ea1b63b070d769ad5496
joeyc004 <joeycmicrosoft@gmail.com>
2026-09-11 22:18:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/pdgeaterysite/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
